7602f7bcb97d7c4d2f540366a9528fb4c7ead41d
Julius <www.hahaasd@outlook.com@example.com>
2020-01-15 08:43:50 +0000

    初始化样式

--- resources/views/static_pages/help.blade.php
+++ resources/views/static_pages/help.blade.php
@@ -3,6 +3,6 @@
   <title>Weibo App</title>
 </head>
 <body>
-  <h1>帮助页</h1>
+  <h1>帮助页12345</h1>
 </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ Route::get('/about', 'StaticPagesController@about');
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
     return view('welcome');
-});
+});*/
