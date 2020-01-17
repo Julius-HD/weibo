814478b0304c3ac310a7fca2ded84d356b3a21c4
Julius <www.hahaasd@outlook.com@example.com>
2020-01-17 07:34:02 +0000

    查看用户列表

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -11,10 +11,15 @@ class UsersController extends Controller
     public function __construct()
     {
         $this->middleware('auth', [            
-            'except' => ['create', 'store']
+            'except' => ['create', 'store', 'index']
         ]);
     }
 
+    public function index()
+    {
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
+    }
     
     public function create()
     {
